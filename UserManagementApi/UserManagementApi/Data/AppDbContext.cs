a23ea8398831735b1049e7a4152b4f14064a4c8a
HH <muhammadharizharun@gmail.com>
2025-10-04 02:40:12 +0800

    commit

---
+++ UserManagementApi/UserManagementApi/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using UserManagementApi.Models;
+
+namespace UserManagementApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }  // Table Users
+    }
+}
